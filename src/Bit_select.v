1ca6d88af4d32cf3f852f2c4bd5ad659e8919fb3
Ahmed Abd El-Azez <52431244+Ahmed-Abd-El-Azez@users.noreply.github.com>
2025-09-15 11:22:56 +0300

    Update Bit_select.v

--- src/Bit_select.v
+++ src/Bit_select.v
@@ -1,30 +1,40 @@
 module Bit_select #(parameter bit_num = 10,parameter num_width = 3)(
-  input en,clk,rst,arst_n,
+  input en,clk,rst,arst_n,tx_en,
   output reg[num_width:0] bit_index,
   output reg busy,done
 );
 
 always @(posedge clk or negedge arst_n) begin
   if (~arst_n) begin
-    bit_index <= 0;
+    bit_index <= {(num_width+1){1'b1}};
     busy <= 0;
     done <= 1;
   end
   else if (rst) begin
-    bit_index <= 0;
+    bit_index <= {(num_width+1){1'b1}};
     busy <= 0;
     done <= 1;
   end
-  else if (en) begin
-    if (bit_index == bit_num-1) begin
-      busy <= 0;
-      done <= 1;
-      bit_index <= {(num_width+1){1'b0}};
-    end
+  else if (~tx_en) begin
+    bit_index = {(num_width+1){1'b1}};
+  end
+  else begin
+    if (bit_index == {(num_width+1){1'b1}}) begin
+        bit_index <= {(num_width+1){1'b0}};
+      end
     else begin
-      busy <= 1;
-      done <= 0;
-      bit_index <= bit_index + 1;
+      if (en) begin
+        if (bit_index == bit_num-1) begin
+          busy <= 0;
+          done <= 1;
+          bit_index <= {(num_width+1){1'b0}};
+        end
+        else begin
+          busy <= 1;
+          done <= 0;
+          bit_index <= bit_index + 1;
+        end
+      end
     end
   end
 end
