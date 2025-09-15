2201f65469771d3a7682a45c245a605607bc2c0d
Ahmed Abd El-Azez <52431244+Ahmed-Abd-El-Azez@users.noreply.github.com>
2025-09-15 11:22:19 +0300

    Update Frame.v

--- src/Frame.v
+++ src/Frame.v
@@ -4,6 +4,6 @@ module Frame #(parameter data_width = 8)(
   output[data_width+1:0] frame_data
 );
 
-assign frame_data = enable ? {1'b1, data_in, 1'b0} : {(data_width+2){1'b0}};
+assign frame_data = enable ? {1'b1, data_in, 1'b0} : {(data_width+2){1'b1}};
 
 endmodule
